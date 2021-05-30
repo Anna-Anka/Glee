ee4a6aa338e117875430192be295832f3d2c8f07
Anna-Anka <anjbekrenewa@gmail.com>
2021-05-30 16:48:01 +0300

    home work 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,14 @@
-$(function(){
+$(function () {
     $('.top-slider__items').slick({
         dots: true,
         arrows: false,
     });
+
+    $('.partners__items').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 5,
+    })
 
     var mixer = mixitup('.products-week__items');
 
